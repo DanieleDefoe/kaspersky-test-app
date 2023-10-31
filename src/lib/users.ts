a82f5ea17f0114e85e8148e259f5d983b176d456
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-10-31 09:05:48 +0300

    fix: replace localhost with ip

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,6 +1,6 @@
 export async function getUsersFromApi(amount = 500) {
   const response = await fetch(
-    `http://localhost:3000/api/users?results=${amount}`
+    `http://127.0.0.1:3000/api/users?results=${amount}`
   );
 
   const { users } = await response.json();
